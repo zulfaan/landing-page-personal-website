65a0d70c082ddb4a45e6e36a8a129e28352cee58
Zulfa Nurfajar <znurfajar@gmail.com>
2023-03-21 14:10:41 +0700

    new changes

---
+++ menu.js
@@ -0,0 +1,18 @@
+const menuBar = document.querySelector(".open");
+const listBar = document.querySelector(".list-header");
+
+menuBar.addEventListener("click", function() {
+  listBar.classList.toggle("active");
+});
+
+const closedBar = document.querySelector(".closed");
+
+closedBar.addEventListener("click", function() {
+    listBar.classList.remove("active");
+  });
+
+// closedBars.forEach(function(closedBar) {
+//   closedBar.addEventListener("click", function() {
+//     closedBar.classList.remove("active");
+//   });
+// });
